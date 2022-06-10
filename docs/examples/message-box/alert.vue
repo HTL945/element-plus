ec8ad870c7241d983285255b7dfe09c8982bee85
HTL945 <91409643+HTL945@users.noreply.github.com>
2022-06-10 11:00:27 +0800

    Update alert.vue

--- docs/examples/message-box/alert.vue
+++ docs/examples/message-box/alert.vue
@@ -9,12 +9,7 @@ import type { Action } from 'element-plus'
 const open = () => {
   ElMessageBox.alert('This is a message', 'Title', {
     confirmButtonText: 'OK',
-    callback: (action: Action) => {
-      ElMessage({
-        type: 'info',
-        message: `action: ${action}`,
-      })
-    },
+ 
   })
 }
 </script>
